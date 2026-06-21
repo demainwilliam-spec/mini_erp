53d35b6d764904cc7b17d670a3214656e5a76c65
William Demain <demainwilliam@gmail.com>
2026-06-21 13:17:34 +0200

    ajout autorisation réact/API

--- api/main.py
+++ api/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from database.db import SessionLocal
 from api.routers import authors, suppliers, categories, books, customers, orders, stock_movements
 from models import author, supplier, category, book, customer, order, stock_movement
@@ -9,6 +10,14 @@ app = FastAPI(
     version="1.0.0"
 )
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(authors.router)
 app.include_router(suppliers.router)
 app.include_router(categories.router)
@@ -16,6 +25,7 @@ app.include_router(books.router)
 app.include_router(customers.router)
 app.include_router(orders.router)
 app.include_router(stock_movements.router)
+
 @app.get("/")
 def root():
     return {"message": "Bienvenue sur le Mini ERP API !"}
